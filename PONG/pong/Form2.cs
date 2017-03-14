15dbbc7fcc41d6567f64296b82c9493eed81f1b8
Wolvue <miningwolv@hotmail.com>
2017-03-14 11:51:33 +0100

    added song, form2 start

--- PONG/pong/Form2.cs
+++ PONG/pong/Form2.cs
@@ -12,9 +12,21 @@ namespace PONG
 {
     public partial class Form2 : Form
     {
+        System.Media.SoundPlayer introsong = new System.Media.SoundPlayer(@"wavs\introsong.wav");
         public Form2()
         {
             InitializeComponent();
+            introsong.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            Form Pong = new Pong();
+            Pong.Show();
+            
+            
         }
     }
 }
